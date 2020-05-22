7a722d80091ce404a2ced959c975bbce555937c5
Joël éeguillon <joel.seguillon@gmail.com>
2020-05-22 18:11:40 +0200

    vagrant refactor

--- addItfIfNotExists.ps1
+++ addItfIfNotExists.ps1
@@ -4,10 +4,16 @@ Param ( [String]$vmName = $(throw "-vmName is required."),
 $countItf=(Get-VMNetworkAdapter -VMName $vmName | Where-Object {$_.MacAddress -match $macAddrCtrlPlane}).count
 
 If ($countItf -ne 1) {
-  echo "Adding itf to vm"
-  Stop-VM -VMName $vmName
+  echo "Stop vm $vmName"
+  Stop-VM -VMName $vmName 
+  sleep 2
+  echo "Add itf  $macAddrCtrlPlane to vm $vmName"
   Add-VMNetworkAdapter -VMName $vmName -SwitchName "Default Switch"  -StaticMacAddress $macAddrCtrlPlane
+  sleep 2
+  echo "Starting again vm  $vmName"
   Start-VM -VMName $vmName
+  sleep 2
+  echo "Vm now ready"
 }
 Else {
   echo "Itf already on this VM"
